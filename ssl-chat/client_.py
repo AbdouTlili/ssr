a5940de210d82f35ff4a2a15b2263158026a5b80
AbdouTlili <ha_tlili@esi.dz>
2021-04-10 16:19:29 +0100

    preject files rebase

---
+++ ssl-chat/client_.py
@@ -0,0 +1,62 @@
+import sys 
+import socket 
+import select
+import os
+from handshake import handshake
+server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+if len(sys.argv) != 5 :
+    print("it should be used like : chat_server.py <ip> <port> <nom_client> <password> ")
+    exit()
+
+
+#getting the ip and port
+IP_addr = str(sys.argv[1])
+Port = int(sys.argv[2])
+Name = str(sys.argv[3])
+password = str(sys.argv[4])
+
+# create a directory where to store keys and received messages !!
+CLIENT_DIR = f".keys_{Name}"
+if not os.path.exists(CLIENT_DIR):
+    os.makedirs(CLIENT_DIR)
+os.chdir(CLIENT_DIR)
+
+#bind the sever to the given ip and port 
+server.connect((IP_addr,Port))
+
+## TODO  call handshake function 
+handshake(server,True, password) 
+
+#while True: 
+#  
+#    # maintains a list of possible input streams 
+#    sockets_list = [sys.stdin, server] 
+#  
+#    """ There are two possible input situations. Either the 
+#    user wants to give manual input to send to other people, 
+#    or the server is sending a message to be printed on the 
+#    screen. Select returns from sockets_list, the stream that 
+#    is reader for input. So for example, if the server wants 
+#    to send a message, then the if condition will hold true 
+#    below.If the user wants to send a message, the else 
+#    condition will evaluate as true"""
+#    read_sockets,write_socket, error_socket = select.select(sockets_list,[],[]) 
+#
+#  
+#    for socks in read_sockets: 
+#        if socks == server: 
+#            message = socks.recv(4096) 
+#            ## decrypt and verify signature  
+#            ## using sharedkey variable 
+#            message = message.decode()
+#            print(message) 
+#            sys.stdout.flush() 
+#        else: 
+#            message = sys.stdin.readline() 
+#            ## encrypt and sign 
+#            ## using sharedkey variable 
+#            message_as_byte = message.encode()
+#            server.send(message_as_byte) 
+#            print("<You>"+message) 
+#            sys.stdout.flush() 
+server.close() 
